b093340ee5a88af42910ca173e841787fa6121b1
André <Andr@200.137.220.94>
2014-06-26 22:05:06 -0300

    -implementando busca por spring ldap

---
+++ src/main/java/br/ufg/reqweb/util/LdapUser.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ufg.reqweb.util;
+
+import org.springframework.ldap.odm.annotations.Attribute;
+import org.springframework.ldap.odm.annotations.Entry;
+import org.springframework.ldap.odm.annotations.Id;
+
+/**
+ *
+ * @author André
+ */
+
+@Entry(objectClasses = { "person", "top" }, base="ou=Users")
+public class LdapUser {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    private String usuario;
+    
+    @Attribute(name = "cn")
+    private String nome;
+    
+    @Attribute(name = "gidNumber")
+    private String grupo;
+    
+    @Attribute(name = "uidNumber")
+    private String uid;
+    
+    @Attribute(name = "uidNumber")
+    private String matricula;
+    
+    @Attribute(name = "uidNumber")
+    private String email;
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the grupo
+     */
+    public String getGrupo() {
+        return grupo;
+    }
+
+    /**
+     * @param grupo the grupo to set
+     */
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    /**
+     * @return the uid
+     */
+    public String getUid() {
+        return uid;
+    }
+
+    /**
+     * @param uid the uid to set
+     */
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
